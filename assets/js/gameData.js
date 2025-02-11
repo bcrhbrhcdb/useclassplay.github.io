fe54ff78348cb7b809b851294d38bfef4aac5d7e
Alex <123518949+bcrhbrhcdb@users.noreply.github.com>
2025-02-11 14:06:11 -0600

    Update gameData.js

--- assets/js/gameData.js
+++ assets/js/gameData.js
@@ -3,9 +3,10 @@ const games = {
     tetris: {
         title: "Twitch Tetris",
       //if you only want to have the game open up in an about:blank, this is not needed.
+        //the id should be the same as the name of the object, so it should be "tetris" for this game.
         url: "game-template.html?id=tetris",
         originalUrl: "https://emulatoros.github.io/gfile/tetris/index.html",
-        image: "../images/proxyandgameimages/gameimages/tetris.png",
+        image: "../img/tetris.png",
         description: "You know what this is, a ripoff of Tetris.",
       //for info insid the game page, you can add this:
       //you can use literal templates for html OR just normal quotes for text.
